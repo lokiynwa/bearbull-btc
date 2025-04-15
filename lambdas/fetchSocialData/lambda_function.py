f1cc290d1abb4a370cc74638d76b6683f4f09119
lokiynwa <125258076+lokiynwa@users.noreply.github.com>
2025-04-15 14:04:04 +0100

    Added filter to make posts more relevant

--- lambdas/fetchSocialData/lambda_function.py
+++ lambdas/fetchSocialData/lambda_function.py
@@ -3,7 +3,6 @@ import json
 import requests
 from base64 import b64encode
 
-# workflow test
 def get_reddit_token():
     client_id = os.environ['REDDIT_CLIENT_ID']
     client_secret = os.environ['REDDIT_CLIENT_SECRET']
@@ -29,14 +28,27 @@ def fetch_subreddit_posts(subreddit, token, limit=100):
     posts = res.json().get("data", {}).get("children", [])
     return [post["data"]["title"] for post in posts]
 
+def is_relevant(title):
+    keywords = [
+        "bitcoin", "btc", "crypto", "halving", "satoshi", "blockchain", "wallet",
+        "coinbase", "binance", "kraken", "hodl", "bull", "bear", "pump", "dump",
+        "buy", "sell", "price", "crash", "moon", "fomo", "etf", "volatility", "market",
+        "regulation", "mining", "trading", "investment", "portfolio", "loss", "gain",
+        "profit", "liquidate", "greed", "fear", "support", "resistance", "chart",
+        "prediction", "correction", "bottom", "top", "trend", "bullish", "bearish"
+    ]
+    title_lower = title.lower()
+    return any(word in title_lower for word in keywords)
+
 def lambda_handler(event, context):
     token = get_reddit_token()
     subreddits = ["Bitcoin", "CryptoCurrency", "CryptoMarkets"]
     
     all_titles = []
     for subreddit in subreddits:
-        titles = fetch_subreddit_posts(subreddit, token, limit=100)
-        all_titles.extend(titles)
+        titles = fetch_subreddit_posts(subreddit, token, limit=200)
+        relevant_titles = [t for t in titles if is_relevant(t)]
+        all_titles.extend(relevant_titles)
 
     return {
         "statusCode": 200,
